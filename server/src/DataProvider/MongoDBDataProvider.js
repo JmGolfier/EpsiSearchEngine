33f753c7478e288ee2d204ebc1c3c5798311b417
epayet <jakspinning@gmail.com>
2014-06-28 18:47:59 +0200

    Server nodejs + mongodb

--- server/src/DataProvider/MongoDBDataProvider.js
+++ server/src/DataProvider/MongoDBDataProvider.js
@@ -2,19 +2,16 @@ var MongoClient = require('mongodb').MongoClient;
 
 var MongoDBDataProvider = function (options) {
     this.databaseName = options.databaseName;
-    this.mongoUri = "mongodb://" + options.url + this.databaseName;
+    this.mongoUri = "mongodb://" + options.url + "/" + this.databaseName;
     this._ensureIndex();
 };
 
 MongoDBDataProvider.prototype.get = function (filter, callback) {
     this._connect(function (collection, closeCallback) {
-        collection.find({$text: {$search: filter.default, $language: "fr"}}, function(err, result) {
+        collection.find({$text: {$search: filter.default, $language: "fr"}}).toArray(function(err, result) {
             if(err) throw err;
-            result.toArray(function (err, result) {
-                if(err) throw err;
-                callback(result);
-                closeCallback();
-            });
+            callback(result);
+            closeCallback();
         });
     });
 };
