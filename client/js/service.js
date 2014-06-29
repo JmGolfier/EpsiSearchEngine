302c9d0773a6fc4dac3cff57f82cb6dceeef31be
epayet <jakspinning@gmail.com>
2014-06-29 19:18:14 +0200

    correct url

--- client/js/service.js
+++ client/js/service.js
@@ -1,15 +1,19 @@
 var searchServices = angular.module('searchServices', ['ngResource']);
 
-searchServices.factory('Search', ['$resource',
-    function($resource){
-        return $resource('http://localhost:8080/search/:query', {}, {
+searchServices.factory('Search', ['$resource', '$location',
+    function($resource, $location){
+        return $resource(getUrl($location.host(), 'search/:query'), {}, {
             query: { method:'GET', isArray:false}
         });
 }]);
 
-searchServices.factory('AutoComplete', ['$resource',
-    function($resource){
-        return $resource('http://localhost:8080/autocomplete', {}, {
+searchServices.factory('AutoComplete', ['$resource', '$location',
+    function($resource, $location){
+        return $resource(getUrl($location.host(), "autocomplete"), {}, {
             query: { method:'GET', isArray:true}
         });
     }]);
+
+function getUrl(host, resource) {
+    return "http://" + host + ":8080/" + resource;
+}
