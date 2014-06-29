6a4261b7590fe62cfa98dd30b5e1fcf5047480a0
epayet <jakspinning@gmail.com>
2014-06-29 17:56:59 +0200

    mieux

--- python/epsi/nosql/init_folder.py
+++ python/epsi/nosql/init_folder.py
@@ -3,5 +3,5 @@ from epsi.nosql.job_runner.job_runner import JobRunner, index_pdf
 
 
 if __name__ == '__main__':
-    # JobRunner().index_folder(sys.argv[1])
-    JobRunner().index_folder("/home/manu/Documents/pdf")
+    JobRunner().index_folder(sys.argv[1])
+    # JobRunner().index_folder("/home/manu/Documents/pdf")
